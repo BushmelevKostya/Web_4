798ebe082cb52638d490bb12b6104c6d7ee8a327
Kostya <kostikbushmelev@gmail.com>
2023-12-18 01:15:11 +0300

    change router

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from "./App";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Start from "./Start";
+import Main from "./Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Start/>,
+    },
+    {
+        path: '/app',
+        element: <Main/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router} />
     </React.StrictMode>
-);
+)
